d784978bfe00148eab68292d8388a1859be1ca4c
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2022-11-03 16:02:59 +0200

    feat: added solution of the TASK 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+ 
+let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), {encoding: 'utf8'});
+let data = '';
+stream.on('readable', function() {
+  let chunk = stream.read();
+  if (chunk != null) {
+    data += chunk;
+  }
+});
+
+stream.on('end', function(){
+  console.log(data)
+});
+
+stream.on('error', error => {
+  console.log('Error: ', error.message)
+});
