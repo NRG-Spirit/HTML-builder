42a86e948b5f3db370a6dd08a4a0ee38fbdd3a15
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2022-11-04 15:10:49 +0200

    feat: added solution of the TASK 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises');
+const path = require('path');
+const process = require('process');
+
+async function getFiles () {
+  try {
+    const files = await fs.readdir(path.join(__dirname, './secret-folder'), {withFileTypes: true});
+      for (const file of files) {
+        if (file.isFile()) {
+          let filePath = path.join(__dirname, './secret-folder', file.name);
+          let ext = path.extname(filePath);
+          let name = file.name.replace(ext, '');
+          let size = (await fs.stat(filePath)).size;
+          process.stdout.write(name + ' - ' + ext.slice(1) + ' - ' + size + 'kb' + '\n');
+        }
+     }; 
+  } catch (err) {
+    process.stdout.write('Error: ', err.message + '\n');
+  }
+};
+
+getFiles();
