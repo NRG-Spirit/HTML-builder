90bc74488274794ae3a2c8e0530f2072edf3231a
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2022-11-06 15:18:44 +0200

    feat: added solution of the TASK 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const { readdir } = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+
+async function getBundle () {
+  try {
+    const files = await readdir(path.join(__dirname, './styles'), {withFileTypes: true});
+    const bundle = fs.WriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+    for (const file of files) {
+        if (file.isFile()) {
+          let filePath = path.join(__dirname, './styles', file.name);
+          let ext = path.extname(filePath);
+          if (ext === '.css') {
+            let stream = new fs.ReadStream(path.join(__dirname, './styles', file.name));
+            stream.pipe(bundle);
+          }
+        }
+     }; 
+  } catch (err) {
+    process.stdout.write('Error: ', err.message + '\n');
+  }
+};
+
+getBundle();
